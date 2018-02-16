10a88e604cb2d58837f470e1b7b31a874ac8c6ae
RimTouiri <rim.touiri@edu.ece.Fr>
2018-02-16 11:04:26 +0100

    Rim lpb

---
+++ GIT/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string b ;
+    cout << "Hello world!" << endl;
+    cout << "My name is Rim " << endl;
+    cout << " What about you ? " << endl;
+    cin >> b;
+    cout << "Hello " << b ;
+    return 0;
+}
